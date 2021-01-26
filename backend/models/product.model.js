c261762ccf594fb103f1db51093cfb666e8ee6ab
Shreeansh <sarojvarshney20@gmail.com>
2021-01-26 17:08:40 +0530

    added product schema

---
+++ backend/models/product.model.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      unique: true,
+      maxlength: [40, "Title can't exceed 40 characters"],
+      trim: true,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+      maxlength: [2000, "Body can't exceed 2000 characters"],
+      text: true,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+      trim: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+    },
+    subs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Sub',
+      },
+    ],
+    quantity: {
+      type: Number,
+      required: [true, 'Quantity is required'],
+    },
+    total_sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: String,
+      enum: ['Yes', 'No'], // only yes and no allowed
+    },
+    color: {
+      type: String,
+      enum: ['Black', 'Brown', 'Silver', 'White', 'Blue'],
+    },
+    brand: {
+      type: String,
+    },
+    // ratings: [
+    //   {
+    //     star: Number,
+    //     postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    //   },
+    // ],
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
