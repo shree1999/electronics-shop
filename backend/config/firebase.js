411f21e2516ad030d50cc8def24ddbd3c2397acb
Shreeansh <sarojvarshney20@gmail.com>
2021-03-26 16:23:59 +0530

    huge blunder rectified

--- backend/config/firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./googleConfig.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://e-commerce-dad49.firebaseio.com",
-});
-
-module.exports = admin;
