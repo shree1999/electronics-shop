6c64bad29d0d9b9f94e06eed21ec4fe2400e085c
Shreeansh <sarojvarshney20@gmail.com>
2021-05-01 13:40:06 +0530

    download pdf invoice possible

---
+++ client/src/components/Cards/ShowPaymentInfo.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export const ShowPaymentInfo = ({ order }) => (
+  <div>
+    <p className="lead small">
+      <strong>Order Id:</strong> {order.paymentIntent.id}
+    </p>
+
+    <p className="lead small">
+      <strong>Amount:</strong>
+      {(order.paymentIntent.amount / 100).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'INR',
+      })}
+    </p>
+
+    <p className="lead small">
+      <strong>Currency:</strong>
+      {order.paymentIntent.currency.toUpperCase()}
+    </p>
+
+    <p className="lead small">
+      <strong>Method:</strong>
+      {order.paymentIntent.payment_method_types[0]}
+    </p>
+
+    <p className="lead small">
+      <strong>Payment:</strong>
+      {order.paymentIntent.status.toUpperCase()}
+    </p>
+
+    <p className="lead small">
+      <strong>Orderd on:</strong>
+      {new Date(order.paymentIntent.created * 1000).toLocaleString()}
+    </p>
+
+    <p className="badge bg-primary text-white">STATUS: {order.orderStatus}</p>
+  </div>
+);
--- client/src/pages/users/History.js
+++ client/src/pages/users/History.js
@@ -1,8 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { getUserOrders } from '../../actions/userAction';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFDownloadLink,
+  PDFViewer,
+} from '@react-pdf/renderer';
 
 import UserNav from '../../components/navs/UserNav';
+import { ShowPaymentInfo } from '../../components/Cards/ShowPaymentInfo';
 
 const History = () => {
   const [products, setProducts] = useState([]);
@@ -23,20 +34,76 @@ const History = () => {
     }
   };
 
-  const showOrderInTable = order => <p>each order and it's products</p>;
+  const showOrderInTable = order => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
 
-  const showEachOrders = () =>
-    products.map((order, i) => (
-      <div key={i} className="m-5 p-3 card">
-        <p>show payment info</p>
-        {showOrderInTable(order)}
-        <div className="row">
-          <div className="col">
-            <p>PDF download</p>
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === 'Yes' ? (
+                <CheckCircleOutlined style={{ color: 'green' }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: 'red' }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  const showDownloadLink = order => (
+    <PDFDownloadLink
+      document={
+        <Document>
+          <Page size="A4">
+            <View>
+              <Text>Section #1</Text>
+              <Text>Section #2</Text>
+            </View>
+          </Page>
+        </Document>
+      }
+      fileName="invoice.pdf"
+      className="btn btn-sm btn-block btn-outline-primary"
+    >
+      Download PDF
+    </PDFDownloadLink>
+  );
+
+  const showEachOrders = () => (
+    <Fragment>
+      {products.map((order, i) => (
+        <div key={i} className="m-5 p-3 card">
+          <ShowPaymentInfo order={order} />
+          {showOrderInTable(order)}
+          <div className="row">
+            <div className="col">
+            {showDownloadLink(order)}
+            </div>
           </div>
         </div>
-      </div>
-    ));
+      ))}
+    </Fragment>
+  );
 
   return (
     <div className="container-fluid">
